c42dcf7a2c7ab63b0531e582e610a19002c84142
Cruz Jesus Rodriguez <isc.cruzjrodriguez@gmail.com>
2024-12-10 10:03:26 -0600

    update directories paths

--- restore_libffmpeg.sh
+++ restore_libffmpeg.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-DIR_LIB="/snap/chromium-ffmpeg/24/chromium-ffmpeg-104707/chromium-ffmpeg"
+DIR_CURRENT="/snap/chromium-ffmpeg/current"
+YAML_FILE="$DIR_CURRENT/snap/snapcraft.yaml"
+versions=$(awk -F': ' '/version:/ {print $2}' $YAML_FILE)
+current_v=$(echo "$versions" | cut -d'-' -f1 | tr -d '"')
+DIR_LIB="${DIR_CURRENT}/chromium-ffmpeg-${current_v}/chromium-ffmpeg"
 DIR_OPERA="/usr/lib/x86_64-linux-gnu/opera"
 FILE_FFMPEG_CONFIG="ffmpeg_preload_config.json"
 
@@ -16,6 +20,7 @@ if [ -d "$DIR_LIB" ]; then
 	  "/usr/lib/chromium-browser/libs/libffmpeg.so"
 	]' | sudo tee "$DIR_OPERA/resources/$FILE_FFMPEG_CONFIG" >/dev/null  
 	#> "$DIR_OPERA/resources/$FILE_FFMPEG_CONFIG"
+	echo "Restart Browser to apply effect."
 else
 	echo "${DIR_LIB} not found. Can not continue"
 fi
